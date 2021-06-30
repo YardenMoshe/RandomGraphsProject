33e887d79682080ac00e4c462afd08a47837a392
YardenMoshe <Yarden9797@gmail.com>
2021-06-30 23:03:36 +0300

    refactor

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ end = time.time()
 print("completed in: %s seconds" % (end - start))
 
 
-#Example showing how to use all distruibtionss
+# # Example showing how to use all distruibtions
 # NValues = [10,100,1000,10000]
 # initial_start = time.time()
 # for N in NValues:
@@ -26,9 +26,11 @@ print("completed in: %s seconds" % (end - start))
 #             for paretoVal in paretoValues:
 #                 weights = get_random_weights(dist, N, paretoVal)
 #                 heavy_weights_matrix = HeavyPathCalculator(weights).calculate_heavy_paths_matrix()
+#                 heaviest_path_weight = heavy_weights_matrix[N-1][N-1]
 #         else:
 #             weights = get_random_weights(dist, N)
 #             heavy_weights_matrix = HeavyPathCalculator(weights).calculate_heavy_paths_matrix()
+#             heaviest_path_weight = heavy_weights_matrix[N-1][N-1]
 #         end=time.time()
 #         print("completed in: %s seconds" % (end - start))
 # final_end = time.time()
