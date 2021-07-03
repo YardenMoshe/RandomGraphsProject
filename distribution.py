38e5449bdaf6e3edc5cd77170347ad58a2ef2ea0
Yarden Moshe <Yarden9797@gmail.com>
2021-07-03 14:56:56 +0300

    refactor

--- distribution.py
+++ distribution.py
@@ -1,7 +1,7 @@
 import math
 import numpy as np
+import numba as nb
 from enum import Enum
-from bounded_pareto_generator import generate_bounded_pareto_random
 
 
 class Distribution(Enum):
@@ -13,7 +13,6 @@ class Distribution(Enum):
 
 
 def get_random_weights(distribution, N, pareto_parameters=(1, 1)):
-
     if distribution == Distribution.EXPONENTIAL:
         return exponential_random(N)
 
@@ -40,6 +39,22 @@ def exponential_random(N):
 
 def binomial_random(N, p):
     return np.reshape(np.random.binomial(1, [p] * N * N), (N, N))
+
+
+@nb.njit(['float64[:,::1](float64,int32,int32)'])
+def generate_bounded_pareto_random(alpha, max_value, N):
+    # according to wikipedia page:
+    # https://en.wikipedia.org/wiki/Pareto_distribution#Bounded_Pareto_distribution
+    # section: 3.5.1
+    n_square = int(math.pow(N, 2))
+    uniform_random_numbers = np.random.random(n_square)[0:n_square]
+    bounded_pareto_samples = np.empty(n_square)
+    i = 0
+    for n in uniform_random_numbers:
+        h = math.pow(max_value, alpha)
+        bounded_pareto_samples[i] = math.pow(-((n * h - n - h) / h), -1 / alpha)
+        i += 1
+    return np.reshape(bounded_pareto_samples, (N, N))
 
 
 def bounded_pareto_random(N, pareto_parameters):
--- main.py
+++ main.py
@@ -8,29 +8,29 @@ from heavy_paths_calculator import calculate_heavy_paths_matrix
 paretoValues = list(itertools.product([0.5, 1, 1.5, 2], [100, 10000, 1000000, 100000000]))
 
 # example showing how to use single distribution and N value
-# N = 10000
-# start = time.time()
-# weights = get_random_weights(Distribution.PARETO, N,paretoValues[0])
-# print(weights)
-# heavy_weights_matrix = calculate_heavy_paths_matrix(weights,N)
-# heaviest_path_weight = heavy_weights_matrix[N-1][N-1]
-# end = time.time()
-# print("completed in: %s seconds" % (end - start))
+N = 10000
+start = time.time()
+weights = get_random_weights(Distribution.PARETO, N,paretoValues[0])
+print(weights)
+heavy_weights_matrix = calculate_heavy_paths_matrix(weights,N)
+heaviest_path_weight = heavy_weights_matrix[N-1][N-1]
+end = time.time()
+print("completed in: %s seconds" % (end - start))
 
 
-# # Example showing how to use all distributions
-NValues = [10000]
-initial_start = time.time()
-for N in NValues:
-    for dist in Distribution:
-        if dist == Distribution.PARETO:
-            for paretoVal in paretoValues:
-                weights = get_random_weights(dist, N, paretoVal)
-                heavy_weights_matrix = calculate_heavy_paths_matrix(weights, N)
-                heaviest_path_weight = heavy_weights_matrix[N - 1][N - 1]
-        else:
-            weights = get_random_weights(dist, N)
-            heavy_weights_matrix = calculate_heavy_paths_matrix(weights, N)
-            heaviest_path_weight = heavy_weights_matrix[N - 1][N - 1]
-final_end = time.time()
-print("final computing time : %s seconds" % (final_end - initial_start))
+# Example showing how to use all distributions
+# NValues = [10000]
+# initial_start = time.time()
+# for N in NValues:
+#     for dist in Distribution:
+#         if dist == Distribution.PARETO:
+#             for paretoVal in paretoValues:
+#                 weights = get_random_weights(dist, N, paretoVal)
+#                 heavy_weights_matrix = calculate_heavy_paths_matrix(weights, N)
+#                 heaviest_path_weight = heavy_weights_matrix[N - 1][N - 1]
+#         else:
+#             weights = get_random_weights(dist, N)
+#             heavy_weights_matrix = calculate_heavy_paths_matrix(weights, N)
+#             heaviest_path_weight = heavy_weights_matrix[N - 1][N - 1]
+# final_end = time.time()
+# print("final computing time : %s seconds" % (final_end - initial_start))
