9121c102dbde512658f1417bf2ffc423b323734e
Yarden Moshe <Yarden9797@gmail.com>
2021-07-04 08:53:26 +0300

    refactor

--- heavy_paths_calculator.py
+++ heavy_paths_calculator.py
@@ -2,7 +2,9 @@ import numpy as np
 import numba as nb
 
 
-@nb.njit(['int64[:,::1](int64[:,::1],int32)','int32[:,::1](int32[:,::1],int32)', 'float64[:,::1](float64[:,::1],int64)'])
+@nb.njit(['int64[:,::1](int64[:,::1],int64)',
+          'int32[:,::1](int32[:,::1],int32)',
+          'float64[:,::1](float64[:,::1],int64)'])
 def calculate_heavy_paths_matrix(input, N):
     output = np.copy(input)
     for x in range(1, N):
@@ -18,7 +20,9 @@ def calculate_heavy_paths_matrix(input, N):
     return output
 
 
-@nb.njit(['int64[:,::1](int64[:,::1],int32)','int32[:,::1](int32[:,::1],int32)', 'float64[:,::1](float64[:,::1],int64)'])
+@nb.njit(['int64[:,::1](int64[:,::1],int64)',
+          'int32[:,::1](int32[:,::1],int32)',
+          'float64[:,::1](float64[:,::1],int64)'])
 def calculate_heavy_paths_diamond_matrix(input, N):
     for y in range(0, N):
         for x in range(0, N):
@@ -34,8 +38,10 @@ def calculate_heavy_paths_diamond_matrix(input, N):
     return output
 
 
-@nb.njit(['UniTuple(int32, 2)(int32[:,::1],int32)','UniTuple(int64, 2)(int32[:,::1],int32)', 'UniTuple(int64, 2)(float64[:,::1],int32)'])
-def getMaximumWeightFromMatrix(input, N):
+@nb.njit(['UniTuple(int32, 2)(int32[:,::1],int32)',
+          'UniTuple(int64, 2)(int64[:,::1],int64)',
+          'UniTuple(int64, 2)(float64[:,::1],int32)'])
+def get_maximum_weight_from_matrix(input, N):
     max = 0
     tuple = (0, 0)
     for y in range(0, N):
--- main.py
+++ main.py
@@ -2,21 +2,20 @@ import itertools
 import time
 
 from distribution import Distribution, get_random_weights
-from heavy_paths_calculator import calculate_heavy_paths_matrix
-
+from heavy_paths_calculator import calculate_heavy_paths_matrix, get_maximum_weight_from_matrix
 
 paretoValues = list(itertools.product([0.5, 1, 1.5, 2], [100, 10000, 1000000, 100000000]))
 
 # example showing how to use single distribution and N value
 N = 10000
 start = time.time()
-weights = get_random_weights(Distribution.PARETO, N,paretoValues[0])
-print(weights)
-heavy_weights_matrix = calculate_heavy_paths_matrix(weights,N)
-heaviest_path_weight = heavy_weights_matrix[N-1][N-1]
+weights = get_random_weights(Distribution.EXPONENTIAL, N)
+heavy_weights_matrix = calculate_heavy_paths_matrix(weights, N)
+heaviest_path_index = get_maximum_weight_from_matrix(heavy_weights_matrix,N)
+heaviest_path_weight = heavy_weights_matrix[heaviest_path_index[0]][heaviest_path_index[1]]
+print(heaviest_path_weight)
 end = time.time()
 print("completed in: %s seconds" % (end - start))
-
 
 # Example showing how to use all distributions
 # NValues = [10000]
